ec2dc201532b547b7117d7ae5ead1bbb8ce4f994
Lina Maria Nieto Alarcon <lina.nieto22243@ucaldas.edu.co>
2024-03-14 16:53:46 -0500

    Modified Index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // poder acceder a leer el archivo .env
 require( 'dotenv' ).config() ;
-const CONNECTION_PORT = process.env || 3005;
+const CONNECTION_PORT = process.env.PORT || 3005;
 app.listen(CONNECTION_PORT,()=>{
     console.log(`Server running on port ${CONNECTION_PORT}`);
 });
